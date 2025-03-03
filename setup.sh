690eb7ae9a3e14dcd21951c4094558ce46e90f7e
Clemens Zagler <c.zagler@noi.bz.it>
2025-03-03 15:54:19 +0100

    alacritty setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DOTFILES=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [[ -z "${XDG_CONFIG_HOME}" ]]; then
+	XDG_CONFIG_HOME=~/.config
+fi
+
 ln -s $DOTFILES/.alacritty.toml ~/.alacritty.toml
+mkdir -p $XDG_CONFIG_HOME/alacritty/
+git clone git@github.com:alacritty/alacritty-theme.git $XDG_CONFIG_HOME/alacritty/alacritty-theme
 
 for arg in $@
 do
