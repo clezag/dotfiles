16c4ee9158db1bb40c19302929758e88102e809e
Clemens Zagler <c.zagler@noi.bz.it>
2025-03-03 15:45:37 +0100

    update alacritty config

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 DOTFILES=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -s $DOTFILES/.alacritty.toml ~/.alacritty.toml
 
-# labwc desktop
-cat pkg.txt | xargs sudo dnf install
-ln -s $DOTFILES/.config/labwc/ ~/.config/labwc
-ln -s $DOTFILES/.config/sfwbar/ ~/.config/sfwbar
+for arg in $@
+do
+	if [[ arg -eq "labwc" ]];
+	then
+		# labwc desktop
+		cat pkg.txt | xargs sudo dnf install
+		ln -s $DOTFILES/.config/labwc/ ~/.config/labwc
+		ln -s $DOTFILES/.config/sfwbar/ ~/.config/sfwbar
+	fi
+
+done
 
