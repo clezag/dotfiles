46d2ed24d6b40920b8bce359e7756fb2e84f33db
Clemens Zagler <c.zagler@noi.bz.it>
2025-06-23 13:45:25 +0200

    refactor package management make modular

--- setup.sh
+++ setup.sh
@@ -4,27 +4,34 @@ if [[ -z "${XDG_CONFIG_HOME}" ]]; then
 	XDG_CONFIG_HOME=~/.config
 fi
 
-# alacritty
-ln -s $DOTFILES/.alacritty.toml ~/.alacritty.toml
-mkdir -p $XDG_CONFIG_HOME/alacritty/
-git clone git@github.com:alacritty/alacritty-theme.git $XDG_CONFIG_HOME/alacritty/alacritty-theme
-
-# vim
-ln -s $DOTFILES/.vimrc ~/.vimrc
-ln -s $DOTFILES/.vim ~/.vim
-
-# zsh
-ln -s $DOTFILES/.zshrc ~/.zshrc
-
 for arg in $@
 do
-	if [[ arg -eq "labwc" ]];
-	then
-		# labwc desktop
-		cat pkg.txt | xargs sudo dnf install
-		ln -s $DOTFILES/.config/labwc/ ~/.config/labwc
-		ln -s $DOTFILES/.config/sfwbar/ ~/.config/sfwbar
-	fi
+	case $arg in
+		labwc)
+			# labwc desktop
+			cat $DOTFILES/pkg.d/labwc | xargs sudo dnf install
+			ln -s $DOTFILES/.config/labwc/ ~/.config/labwc
+			ln -s $DOTFILES/.config/sfwbar/ ~/.config/sfwbar
+			;;
 
+		term)
+			cat $DOTFILES/pkg.d/term | xargs sudo dnf install
+
+			# vim
+			ln -s $DOTFILES/.vimrc ~/.vimrc
+			ln -s $DOTFILES/.vim ~/.vim
+
+			# zsh
+			ln -s $DOTFILES/.zshrc ~/.zshrc
+			;;
+		gui)
+			cat $DOTFILES/pkg.d/gui | xargs sudo dnf install
+
+			# alacritty
+			ln -s $DOTFILES/.alacritty.toml ~/.alacritty.toml
+			mkdir -p $XDG_CONFIG_HOME/alacritty/
+			git clone git@github.com:alacritty/alacritty-theme.git $XDG_CONFIG_HOME/alacritty/alacritty-theme
+			;;
+	esac
 done
 
